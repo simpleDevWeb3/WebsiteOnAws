c160035558cb0b342da50f6f7b6671d1835358d9
LI <g64bit507@gmail.com>
2025-12-06 15:13:43 +0800

    inititlize project

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cjszlggbmlt7.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee_db"
+custombucket = "lmf-bucket-2"
+customregion = "us-east-1"
+
